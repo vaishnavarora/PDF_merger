21650f1e5be135ce49edbf459691b471fd600452
Vaishnav Arora <vaishgabha@gmail.com>
2024-03-01 23:56:30 +0530

    Create PDF_merger.py

---
+++ PDF_merger.py
@@ -0,0 +1,12 @@
+import PyPDF2
+import sys
+template = PyPDF2.PdfFileReader(open('dummy.pdf','rb'))
+watermark = PyPDF2.PdfFileReader(open('wtr.pdf','rb'))
+output = PyPDF2.PdfFileWriter()
+for i in range(template.getNumPages()):
+	page = template.getPage(i)
+	page.mergePage(watermark.getPage(0))
+	output.addPage(page)
+
+	with open('watermarked.pdf','wb') as file:
+		output.write(file)
